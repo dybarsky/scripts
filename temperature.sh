720e3808f0e4e96c8e8a638a8ee8a7b6c7088271
Maksym Dybarskyi <maksym.dybarskyi@gymondo.de>
2022-01-19 16:30:18 +0100

    Add cpu temp tui

---
+++ temperature.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#setterm --cursor off
+length=16
+height=4
+
+printf "\033[2J"
+while true; do 
+	cols=$(tput cols)
+	lines=$(tput lines)
+	column=$(((((($cols - $length)) / 2)) + 1))
+	line=$(((($lines / 2)) - $(($height / 2)) + 1))
+	printf "\033[${line};0H"		
+	sensors \
+		| grep -A 0 'Core' \
+		| cut -c17-20 \
+		| awk '{print "\033[0;31m" $0 "\033[0m°C"}' \
+		| nl -w2 -s ':  ' \
+		| awk '{print "Core" $0}' \
+		| while read line; do 
+			printf "\033[${column}C$line\n"
+		done
+	sleep 1
+done
