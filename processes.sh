37f0c03f0491566c976f6a848d53699c994e19b2
Maksym Dybarskyi <maksym.dybarskyi@gymondo.de>
2022-01-31 17:05:30 +0100

    Add colors for processes names

--- processes.sh
+++ processes.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-watch -n 1 -t "echo ''; ps -Ao comm,pcpu,pmem:6,etime:10 --sort=-pcpu | tail -n +2 | head -n 4 |  sed -e 's/.*/   &/'"
+watch -n 1 -t -c "
+	echo '';
+	ps -Ao comm:15,pcpu,pmem:6,etime:10 --sort=-pcpu | 
+	tail -n +2 |
+ 	head -n 4 |
+ 	sed 's#.*# &#' |
+ 	sed 's#.*/# #' | 
+ 	sed 's#[^[:digit:]]\+#\x1b[37;2m & \x1b[0m#'
+"
+# sed 's/.*\/[^[:digit:]]\+/\x1b[37;2m & \x1b[0m/'
+# sed 's/[^[:digit:]]\+/\x1b[37;2m&\x1b[0m/'
+# sed 's/[a-zA-Z/]\+/\x1b[31m&\x1b[0m/'
+# sed -e 's/[a-zA-Z]+/\x1b[31m&\x1b[0m/'
