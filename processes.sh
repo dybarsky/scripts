6083332342830350941bf693cbcf8f8c5658889b
Maksym Dybarskyi <maksym.dybarskyi@gymondo.com>
2023-02-21 13:31:28 +0100

    simplify processes.sh

--- processes.sh
+++ processes.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 watch -n 1 -t -c "
 	echo '';
-	ps -Ao comm:15,pcpu,pmem:6,etime:10 --sort=-pcpu | 
+	ps -Ao comm:6,pcpu --sort=-pcpu | 
 	tail -n +2 |
- 	head -n 4 |
+ 	head -n 5 |
  	sed 's#.*# &#' |
  	sed 's#.*/# #' | 
  	sed 's#[^[:digit:]]\+#\x1b[37;2m & \x1b[0m#'
